ad1c639bf2d0d2368e3b6c8ffd0d05918ba746f7
Imran Sah <imransah1623@gmail.com>
2021-11-25 16:00:36 +0530

    firebase-init

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import { useState } from 'react'
-import './App.css';
-import Calendar from 'react-calendar';
-import { Input, Button } from '@mui/material';
+import { useState } from "react";
+import "./App.css";
+import Calendar from "react-calendar";
+import { Input, Button } from "@mui/material";
+import { db } from "./firebase";
 
 function App() {
   const [balance, setbalance] = useState(5000);
 
-  const [text, gettext] = useState("");  //todo and the expences
+  const [text, gettext] = useState(""); //todo and the expences
   const [money, getmoney] = useState(0);
 
   const [data, getdata] = useState([]); //database
@@ -19,38 +20,50 @@ function App() {
   //adding todo and the expences to the database and update the balance
 
   function adddata() {
-    getdata([...data, { money, text }])
-    getkey(JSON.stringify(date).slice(1, 11))
-    setbalance((prev) => prev - money)
-    getwholedata([...wholedata, { key: data }])
+    getdata([...data, { money, text }]);
+    getkey(JSON.stringify(date).slice(1, 11));
+    setbalance((prev) => prev - money);
+    getwholedata([...wholedata, { key: data }]);
   }
 
+  console.log(db);
   console.log(data);
   console.log(wholedata);
   console.log(key);
   return (
     <div className="App">
       <div className="header">
-        <div className="username">
-          User1
-        </div>
+        <div className="username">User1</div>
         <div className="balance">
           Balance : <span>{balance}</span>
         </div>
       </div>
       <div className="content">
         <div className="calander">
-          <Calendar
-            onChange={onChange}
-            value={date}
-            minDetail="month"
-          />
+          <Calendar onChange={onChange} value={date} minDetail="month" />
         </div>
         <div className="expenses">
-          <div className="container"><span className="date">{date.getDate()}/{date.getMonth()}</span><br />
-            Details<Input placeholder="Movies,Outting..." id="textinputs" onChange={(e) => gettext(e.target.value)} type="text" />
-            ₹<Input id="expenseinput" onChange={(e) => getmoney(e.target.value)} type="number" />
-            <Button onClick={adddata} variant="contained">Add</Button>
+          <div className="container">
+            <span className="date">
+              {date.getDate()}/{date.getMonth()}
+            </span>
+            <br />
+            Details
+            <Input
+              placeholder="Movies,Outting..."
+              id="textinputs"
+              onChange={(e) => gettext(e.target.value)}
+              type="text"
+            />
+            ₹
+            <Input
+              id="expenseinput"
+              onChange={(e) => getmoney(e.target.value)}
+              type="number"
+            />
+            <Button onClick={adddata} variant="contained">
+              Add
+            </Button>
             <hr />
           </div>
         </div>
